6cf51a2f2dda59c6b3ba3a8ba0206f9b8dfcd227
Anthony J. Clark <ajc@>
2018-01-30 12:27:58 -0600

    Removed junk files.

--- src/extras/bnn/build_sweep.sh
+++ src/extras/bnn/build_sweep.sh
@@ -4,4 +4,6 @@ COMPILER="clang++"
 CPP_FLAGS="-std=c++14 -Wall -Wextra -O3"
 INC_DIRS="-isystem/usr/local/include/eigen3"
 
-$COMPILER $CPP_FLAGS $INC_DIRS bnn_sweep.cpp -o bnn_sweep
+mkdir -p bin
+
+$COMPILER $CPP_FLAGS $INC_DIRS bnn_sweep.cpp -o bin/bnn_sweep

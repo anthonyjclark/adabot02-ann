ab43a25487cfe039fd4e949dd33f6fabc9e6ab2a
Anthony J. Clark <ajc@>
2018-02-01 16:04:50 -0600

    Working on plots.

--- src/extras/bnn/bnn_sweep.cpp
+++ src/extras/bnn/bnn_sweep.cpp
@@ -35,25 +35,26 @@ int main(int argc, char const *argv[])
     }
 
     // Sweep the input space
-    BNN nn(NI, NO, weights, act);
+    BNN bnn(NI, NO, weights, act);
     constexpr double STEP_SIZE = 0.1;
     Eigen::MatrixXd nn_input(1, NI);
 
-    cout << "angle,angular error,linear error,left speed,right speed,extension" << endl;
+    cout << "Angle,Angular Error,Linear Error,Left Speed,Right Speed,Extension" << endl;
     for (double angle = 0; angle <= 1; angle += STEP_SIZE) {
         for (double angular = 0; angular <= 1; angular += STEP_SIZE) {
             for (double linear = 0; linear <= 1; linear += STEP_SIZE) {
 
-                nn_input(0) = angle * 3.1415926;
+                nn_input(0) = angle;
                 nn_input(1) = angular;
                 nn_input(2) = linear;
 
-                auto nn_output = nn.activate(nn_input);
+                Eigen::MatrixXd nn_output = bnn.activate(nn_input);
+                Eigen::MatrixXd nn_output_clamped = nn_output.array().min(1).max(0);
 
                 cout << angle << "," << angular << "," << linear
-                     << "," << nn_output(0)
-                     << "," << nn_output(1)
-                     << "," << nn_output(2) << endl;
+                     << "," << nn_output_clamped(0)
+                     << "," << nn_output_clamped(1)
+                     << "," << nn_output_clamped(2) << endl;
             }
         }
     }
